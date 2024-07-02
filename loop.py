53888640e6dfbd1432f6548ea58f0ef4d662adbf
enje <ghdscvn12345@gmail.com>
2024-07-02 13:31:54 +0300

    improve lottery and started paint

--- loop.py
+++ loop.py
@@ -2,14 +2,14 @@
 # sandwich = 0
 
 # # повторять пока количество бутербродов меньше 6
-# # while sandwich < 6:
-# #     sandwich += 1
-# #     print(f"Сделали {sandwich} бутер")
+# while sandwich < 6:
+#     sandwich += 1
+#     print(f"Сделали {sandwich} бутер")
 
-# # counter = 10
-# # while counter <= 20:
-# #     print(counter)
-# #     counter += 1
+# counter = 10
+# while counter <= 20:
+#     print(counter)
+#     counter += 1
 
 # counter = 100
 # summa = 0
@@ -19,11 +19,15 @@
 #     factorial *= counter
 #     print(counter)
 #     counter -= 1
-#     # time.sleep(1)
-# # print("Поехали!")
+# #     # time.sleep(1)
+# # # print("Поехали!")
 # print(f"Сумма чисел = {summa}")
 # print(f"Факториал = {factorial}")
 
 
-lst = [1, 2, 3, 4, 5]
-print(lst[:2] + ["pop"] + lst[2:])
+# lst = [1, 2, 3, 4, 5]
+# print(lst[:2] + ["pop"] + lst[2:])
+
+
+for i in range(5, 0, -1):
+    print(i)
--- lottery.py
+++ lottery.py
@@ -1,11 +1,28 @@
 import random
 
+print(f"Компьютер кидает кубик, твоя задача - угадать, сколько на нем выпадет точек")
 
-secret_number = random.randint(1, 10)
-my_number = input("Введите ваше число от 1 до 10: ")# -> строка
-my_number = int(my_number) # ->  число
-if my_number == secret_number:
-    print(f"Ты угадал!")
-else:
-    print(f"Победитель лотереи: {secret_number}")
-    print(f"Ваше число: {my_number}")
+points = 10
+
+
+while points > 0:
+    print(f'у вас {points} очков')
+    secret_number = random.randint(1, 6)
+    my_number = input("Введите ваше число от 1 до 6: ")# -> строка
+    my_number = int(my_number) # ->  число
+    bet = int(input('ваша ставка: '))
+    if bet > points:
+        print('так  нельзя, у вас столько нет')
+        continue
+    if my_number == secret_number:
+        print(f"Ты угадала!")
+        points += bet
+        print(f"Ставка выиграна")
+    else:
+        points -= bet
+        print(f"На кубике выпало: {secret_number}")
+        print(f"Ставка проиграна")
+    answer = input("хотите продолжить игру?? ")
+    if answer == "нет":
+        break
+print(f"Ваш результат: {points} очков")
